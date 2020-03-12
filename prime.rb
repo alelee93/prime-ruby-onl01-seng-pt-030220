6c4ee7aa10db861c254b59ae299ffee648363443
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-12 02:26:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(num)
+  
+  if num < 2 
+    return false 
+  else
+  
+    for i in 2..(num -1)
+      if num.abs() % i == 0
+       return false
+      end
+     end
+    end
+  return true
+end
